e567dc76331dbcc36100d70373735ab754c62dd7
cttdev <cttewari@gmail.com>
2020-04-04 21:11:51 -0700

    Fixing checkstyle errors.

--- wpilibj/src/test/java/edu/wpi/first/wpilibj/controller/LTVDiffDriveControllerTest.java
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/controller/LTVDiffDriveControllerTest.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.wpilibj.math.StateSpaceUtil;
 import edu.wpi.first.wpilibj.system.LinearSystem;
+import edu.wpi.first.wpilibj.system.RungeKutta;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVelocitySystemConstraint;
@@ -25,13 +26,13 @@ import edu.wpi.first.wpiutil.math.MatBuilder;
 import edu.wpi.first.wpiutil.math.Matrix;
 import edu.wpi.first.wpiutil.math.MatrixUtils;
 import edu.wpi.first.wpiutil.math.Nat;
-import edu.wpi.first.wpilibj.system.RungeKutta;
-import edu.wpi.first.wpiutil.math.numbers.N1;
-import edu.wpi.first.wpiutil.math.numbers.N2;
 import edu.wpi.first.wpiutil.math.numbers.N10;
+import edu.wpi.first.wpiutil.math.numbers.N2;
+import edu.wpi.first.wpiutil.math.numbers.N1;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SuppressWarnings({"LocalVariableName", "PMD.AvoidInstantiatingObjectsInLoops", "MemberName"})
 class LTVDiffDriveControllerTest {
   private Matrix<N10, N1> x;
   private Matrix<N2, N1> u;
@@ -42,15 +43,17 @@ class LTVDiffDriveControllerTest {
   private double totalTime;
 
   @BeforeEach
-  void setUp(){
-    final LinearSystem<N2, N2, N2> plant = LinearSystem.identifyDrivetrainSystem(3.02, 0.642, 1.382, 0.08495, 10);
+  void setUp() {
+    final LinearSystem<N2, N2, N2> plant = LinearSystem.identifyDrivetrainSystem(
+            3.02, 0.642, 1.382, 0.08495, 10);
     var kinematics = new DifferentialDriveKinematics(1);
 
     controller = new LTVDiffDriveController(
             plant,
             new MatBuilder<>(Nat.N5(), Nat.N1()).fill(0.0625, 0.125, 2.5, 0.95, 0.95),
             new MatBuilder<>(Nat.N2(), Nat.N1()).fill(12.0, 12.0),
-            new MatBuilder<>(Nat.N10(), Nat.N1()).fill(0.002, 0.002, 0.0001, 1.5, 1.5, 0.5, 0.5, 10.0, 10.0, 2.0),
+            new MatBuilder<>(Nat.N10(), Nat.N1()).fill(
+                    0.002, 0.002, 0.0001, 1.5, 1.5, 0.5, 0.5, 10.0, 10.0, 2.0),
             new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.0001, 0.005, 0.005),
             new MatBuilder<>(Nat.N6(), Nat.N1()).fill(0.005, 0.005, 0.005, 0.0001, 1.5, 1.5),
             kinematics,
@@ -71,7 +74,6 @@ class LTVDiffDriveControllerTest {
   }
 
   @Test
-  @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
   void trackingTest() {
     controller.reset(new Pose2d());
     controller.setMeasuredLocalOutputs(0.0, 0.0, 0.0);
@@ -82,7 +84,7 @@ class LTVDiffDriveControllerTest {
     x = MatrixUtils.zeros(Nat.N10(), Nat.N1());
     u = MatrixUtils.zeros(Nat.N2(), Nat.N1());
 
-    for (double i = 0; i < (totalTime); i = i + kDt) {
+    for (double i = 0; i < totalTime; i = i + kDt) {
       var y = controller.getLocalMeasurementModel(x, MatrixUtils.zeros(Nat.N2(), Nat.N1()));
 
       controller.setMeasuredLocalOutputs(y.get(0, 0), y.get(1, 0), y.get(2, 0));
@@ -99,7 +101,6 @@ class LTVDiffDriveControllerTest {
   }
 
   @Test
-  @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
   void trackingTestGlobal() {
     controller.reset(new Pose2d());
     controller.setMeasuredLocalOutputs(0.0, 0.0, 0.0);
@@ -110,12 +111,15 @@ class LTVDiffDriveControllerTest {
     x = MatrixUtils.zeros(Nat.N10(), Nat.N1());
     u = MatrixUtils.zeros(Nat.N2(), Nat.N1());
 
-    for (double i = 0; i < (totalTime); i = i + kDt) {
+    for (double i = 0; i < totalTime; i = i + kDt) {
       var y = controller.getGlobalMeasurementModel(x, MatrixUtils.zeros(Nat.N2(), Nat.N1()));
 
-      y.plus(StateSpaceUtil.makeWhiteNoiseVector(Nat.N6(), new MatBuilder<>(Nat.N6(), Nat.N1()).fill(0.005, 0.005, 0.005, 0.0001, 1.5, 1.5)));
+      y.plus(StateSpaceUtil.makeWhiteNoiseVector(Nat.N6(),
+              new MatBuilder<>(Nat.N6(), Nat.N1()).fill(0.005, 0.005, 0.005, 0.0001, 1.5, 1.5)));
 
-      controller.setMeasuredGlobalOutputs(y.get(0, 0), y.get(1, 0), y.get(2, 0), y.get(3, 0), y.get(4, 0), y.get(5, 0));
+      controller.setMeasuredGlobalOutputs(y.get(0, 0), y.get(1, 0),
+              y.get(2, 0), y.get(3, 0), y.get(4, 0), y.get(5, 0));
+
       controller.setMeasuredInputs(u.get(0, 0), u.get(1, 0));
 
       var inputs = controller.calculate(trajectory.sample(i));
@@ -129,11 +133,10 @@ class LTVDiffDriveControllerTest {
   }
 
   @Test
-  @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
   void trackingTestNoise() {
     controller.reset(new Pose2d());
     controller.setMeasuredLocalOutputs(0.0, 0.0, 0.0);
-    controller.setTolerance(new Pose2d(0.75, 0.75, new Rotation2d(0.5)), 2.0);
+    controller.setTolerance(new Pose2d(1, 1, new Rotation2d(0.5)), 3.0);
 
     double kDt = 0.02;
 
@@ -141,12 +144,14 @@ class LTVDiffDriveControllerTest {
     u = MatrixUtils.zeros(Nat.N2(), Nat.N1());
 
 
-    for (double i = 0; i < (totalTime); i = i + kDt) {
-      kDt += StateSpaceUtil.makeWhiteNoiseVector(Nat.N1(), new MatBuilder<>(Nat.N1(), Nat.N1()).fill(0.001)).get(0, 0);
+    for (double i = 0; i < totalTime; i = i + kDt) {
+      kDt += StateSpaceUtil.makeWhiteNoiseVector(Nat.N1(),
+              new MatBuilder<>(Nat.N1(), Nat.N1()).fill(0.0005)).get(0, 0);
 
       var y = controller.getLocalMeasurementModel(x, MatrixUtils.zeros(Nat.N2(), Nat.N1()));
 
-      y.plus(StateSpaceUtil.makeWhiteNoiseVector(Nat.N3(), new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.0001, 0.005, 0.005)));
+      y.plus(StateSpaceUtil.makeWhiteNoiseVector(Nat.N3(),
+              new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.0001, 0.005, 0.005)));
 
       controller.setMeasuredLocalOutputs(y.get(0, 0), y.get(1, 0), y.get(2, 0));
       controller.setMeasuredInputs(u.get(0, 0), u.get(1, 0));
