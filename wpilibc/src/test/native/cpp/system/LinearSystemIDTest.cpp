40214bd194ebb4b208294fe949b215a16abf495e
Matt <matthew.morley.ca@gmail.com>
2020-04-27 14:03:35 -0700

    [wpilib] Add LinearSystem and Discretization classes
    This includes LinearSystem, classes to create LinearSystems, and Discretization classes.

---
+++ wpilibc/src/test/native/cpp/system/LinearSystemIDTest.cpp
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <frc/system/LinearSystem.h>
+#include <gtest/gtest.h>
+
+#include "frc/system/plant/LinearSystemId.h"
+
+TEST(LinearSystemIDTest, IdentifyVelocitySystem) {
+  double kv = 1.0;
+  double ka = 0.5;
+  auto system = frc::LinearSystemId::IdentifyVelocitySystem(kv, ka);
+}
