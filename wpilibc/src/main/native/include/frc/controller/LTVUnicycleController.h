fc253481a574db0a7808bee495306379ab4f3726
cttdev <cttewari@gmail.com>
2020-07-19 23:42:15 -0700

    Fixes to docs.

--- wpilibc/src/main/native/include/frc/controller/LTVUnicycleController.h
+++ wpilibc/src/main/native/include/frc/controller/LTVUnicycleController.h
@@ -32,15 +32,17 @@ class LTVUnicycleController {
   /**
    * Construct a LTV Unicycle Controller.
    *
-   * @param qElms     The maximum desired error tolerance for the robot's state,
-   * in the form [X, Y, Heading]^T. Units are meters and radians.
-   * @param rElms     The maximum desired control effort by the feedback
-   * controller, in the form [vMax, wMax]^T. Units are meters per second and
-   *                  radians per second. Note that this is not the maximum
-   * speed of the robot, but rather the maximum effort the feedback controller
-   *                  should apply on top of the trajectory feedforward.
+   * @param Qelems    The maximum desired error tolerance for the robot's state,
+   *                  in the form [X, Y, Heading]^T. Units are meters and
+   *                  radians.
+   * @param Relems    The maximum desired control effort by the feedback
+   *                  controller, in the form [vMax, wMax]^T. Units are meters
+   *                  per second and radians per second. Note that this is
+   *                  not the maximum speed of the robot, but rather the maximum
+   *                  effort the feedback controller should apply on top of the
+   *                  trajectory feedforward.
    * @param dtSeconds The nominal dt of this controller. With command based this
-   * is 0.020.
+   *                  is 0.020.
    */
   LTVUnicycleController(const std::array<double, 3>& Qelems,
                         const std::array<double, 2>& Relems,
@@ -49,18 +51,20 @@ class LTVUnicycleController {
   /**
    * Construct a LTV Unicycle Controller.
    *
-   * @param qElms     The maximum desired error tolerance for the robot's state,
-   * in the form [X, Y, Heading]^T. Units are meters and radians.
+   * @param Qelems    The maximum desired error tolerance for the robot's state,
+   *                  in the form [X, Y, Heading]^T. Units are meters and
+   *                  radians.
    * @param rho       A weighting factor that balances control effort and state
-   * excursion. Greater values penalize state excursion more heavily. 1 is a
-   * good starting value.
-   * @param rElms     The maximum desired control effort by the feedback
-   * controller, in the form [vMax, wMax]^T. Units are meters per second and
-   *                  radians per second. Note that this is not the maximum
-   * speed of the robot, but rather the maximum effort the feedback controller
-   *                  should apply on top of the trajectory feedforward.
+   *                  excursion. Greater values penalize state excursion
+   *                  more heavily. 1 is a good starting value.
+   * @param Relems    The maximum desired control effort by the feedback
+   *                  controller, in the form [vMax, wMax]^T. Units are meters
+   *                  per second and radians per second. Note that this is not
+   *                  the maximum speed of the robot, but rather the maximum
+   *                  effort the feedback controller should apply on top of the
+   *                  trajectory feedforward.
    * @param dtSeconds The nominal dt of this controller. With command based this
-   * is 0.020.
+   *                  is 0.020.
    */
   LTVUnicycleController(const std::array<double, 3>& Qelems, const double rho,
                         const std::array<double, 2>& Relems,
@@ -91,13 +95,14 @@ class LTVUnicycleController {
    *
    * @param currentPose                   The current position of the robot.
    * @param currentLinearVelocity         The current linear velocity of the
-   * robot. this can be determined by averaging the measured left and right
-   * wheel velocities.
+   *                                      robot. this can be determined by
+   *                                      averaging the measured left and
+   *                                      right wheel velocities.
    * @param poseRef                       The desired pose of the robot.
    * @param linearVelocityRefMetersPerSec The desired linear velocity of the
-   * robot.
+   *                                      robot.
    * @param angularVelocityRefRadPerSec   The desired angular velocity of the
-   * robot.
+   *                                      robot.
    * @return The next calculated output.
    */
   ChassisSpeeds Calculate(const Pose2d& currentPose,
@@ -121,7 +126,7 @@ class LTVUnicycleController {
    *                              this can be determined by averaging the
    *                              measured left and right wheel velocities.
    * @param desiredState          The desired pose, linear velocity, and angular
-   * velocity from a trajectory.
+   *                              velocity from a trajectory.
    */
   ChassisSpeeds Calculate(const Pose2d& currentPose,
                           units::meters_per_second_t currentLinearVelocity,
