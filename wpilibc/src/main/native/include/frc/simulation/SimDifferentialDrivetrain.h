5278258084c32f46b12e7314612cded7147fcf2d
Claudius Tewari <cttdev@users.noreply.github.com>
2020-08-27 19:24:30 -0700

    C++ diff drive sim (#27)
    * Adding c++ sim diff drive.
    
    * SimBattery fix.

--- wpilibc/src/main/native/include/frc/simulation/SimDifferentialDrivetrain.h
+++ wpilibc/src/main/native/include/frc/simulation/SimDifferentialDrivetrain.h
@@ -7,10 +7,69 @@
 
 #pragma once
 
+#include <frc/system/plant/DCMotor.h>
+#include <frc/kinematics/DifferentialDriveKinematics.h>
+#include <frc/system/LinearSystem.h>
+
+#include "Eigen/Core"
+#include "units/time.h"
+#include "units/voltage.h"
+
 namespace frc {
 namespace sim {
 
-class SimDifferentialDrivetrain {};
+template <int N>
+using Vector = Eigen::Matrix<double, N, 1>;
 
+class SimDifferentialDrivetrain {
+ public:
+   SimDifferentialDrivetrain(LinearSystem<2, 2, 2>& plant,
+                             DifferentialDriveKinematics& kinematics,
+                             DCMotor leftGearbox, double leftGearing,
+                             DCMotor rightGearbox, double rightGearing);
+
+   void SetInputs(units::volt_t leftVoltage, units::volt_t rightVoltage);
+
+   void Update(units::second_t dt);
+
+   double GetState(int state);
+
+   Vector<10> GetState();
+
+   Rotation2d GetHeading();
+
+   Pose2d GetEstimatedPosition();
+
+   units::ampere_t GetCurrentDrawAmps();
+
+   Vector<10> Dynamics(const Vector<10>& x, const Vector<2>& u);
+
+   class State {
+    public:
+        static constexpr int kX = 0;
+        static constexpr int kY = 1;
+        static constexpr int kHeading = 2;
+        static constexpr int kLeftVelocity = 3;
+        static constexpr int kRightVelocity = 4;
+        static constexpr int kLeftPosition = 5;
+        static constexpr int kRightPosition = 6;
+        static constexpr int kLeftVoltageError = 7;
+        static constexpr int kRightVoltageError = 8;
+        static constexpr int kAngularVelocityError = 9;
+   };
+    
+ private:
+   LinearSystem<2, 2, 2> m_plant; 
+   units::meter_t m_rb;
+
+   DCMotor m_leftMotor;
+   DCMotor m_rightMotor;
+
+   double m_leftGearing;
+   double m_rightGearing;
+
+   Vector<10> m_x;
+   Vector<2> m_u;
+};
 }  // namespace sim
 }  // namespace frc
